0524b4a50c523c290030beb80a3bd98add30429b
James Kirsch <jameskirschaf@gmail.com>
2018-01-03 15:48:42 -0800

    Added Alternating preChipDrop Color - Updating dev From Conflict Errors

--- game.js
+++ game.js
@@ -66,56 +66,60 @@ function GameBoard() {
 }
 
 
-GameBoard.prototype.columnClicked = function(event){
-  //check to make sure only one animation at a time
-  if(this.pickedColumn || this.gameOver){
-    return
-  }
-  this.pickedColumn = true;
-  //get which column was clicked from the class
-  var targetColumn = $(event.target).attr("class").split(" ")[1];
-  var column = targetColumn[targetColumn.length - 1];
-  //drop the chip
-  this.chipDrop(column);
-}
-
-GameBoard.prototype.fillBoard = function(height, width){
-  for(var i = 0 ; i < width ; i++){
-    var row = [];
-    for(var j=0 ; j < height ; j++){
-      var newChip = new Chip(false, null);
-      row.push(newChip);
+GameBoard.prototype.columnClicked = function (event) {
+    //check to make sure only one animation at a time
+    if (this.pickedColumn || this.gameOver) {
+        return
     }
-    this.board.push(row);
+    this.pickedColumn = true;
+    //get which column was clicked from the class
+    var targetColumn = $(event.target).attr("class").split(" ")[1];
+    var column = targetColumn[targetColumn.length - 1];
+    //drop the chip
+    this.chipDrop(column);
 };
 
-GameBoard.prototype.chipDrop = function(column){
-  for(var row = this.board.length-1 ; row >= 0 ; row--){
-    if(!this.board[row][column].filled){
-      this.board[row][column].filled = true;
-      this.board[row][column].player = this.currentPlayer.name;
-      this.pickedColumn = false; //allows the next player to now click a column
-      var position = row + " " + column;
-      this.showChip(column, row);
-      //alternates players
-      if(this.currentPlayer === this.playerOne){
-        this.currentPlayer.picks.push(position)
-        this.checkIfWinner(this.currentPlayer.picks)
-        this.currentPlayer = this.playerTwo;
-      } else {
-        this.currentPlayer.picks.push(position)
-        this.checkIfWinner(this.currentPlayer.picks)
-        this.currentPlayer = this.playerOne;
-      }
-      return;
+GameBoard.prototype.fillBoard = function (height, width) {
+    for (var i = 0; i < width; i++) {
+        var row = [];
+        for (var j = 0; j < height; j++) {
+            var newChip = new Chip(false, null);
+            row.push(newChip);
+        }
+        this.board.push(row);
+    }
+};
+
+GameBoard.prototype.chipDrop = function (column) {
+    for (var row = this.board.length - 1; row >= 0; row--) {
+        if (!this.board[row][column].filled) {
+            this.board[row][column].filled = true;
+            this.board[row][column].player = this.currentPlayer.name;
+            this.pickedColumn = false; //allows the next player to now click a column
+            var position = row + " " + column;
+            this.showChip(column, row);
+            //alternates players
+            if (this.currentPlayer === this.playerOne) {
+                this.currentPlayer.picks.push(position)
+                this.checkIfWinner(this.currentPlayer.picks)
+                this.currentPlayer = this.playerTwo;
+            } else {
+                this.currentPlayer.picks.push(position)
+                this.checkIfWinner(this.currentPlayer.picks)
+                this.currentPlayer = this.playerOne;
+            }
+            return;
+        }
     }
 };
 
 GameBoard.prototype.showChip = function (column, row) {
     console.log(this.currentPlayer);
     $gameSquare = $('.square.col' + column + '.row' + row);
-    $gameSquare.css({"background-image": "url('../c12.17hacka_connect_athon/images/disks/"+this.currentPlayer.playerColor+"Disk.png')",
-                     "background-size": "contain"})
+    $gameSquare.css({
+        "background-image": "url('../c12.17hacka_connect_athon/images/disks/" + this.currentPlayer.playerColor + "Disk.png')",
+        "background-size": "contain"
+    })
         .addClass("fallToRow" + row);
 };
 
@@ -125,52 +129,57 @@ GameBoard.prototype.getPlayerNames = function () {
     this.currentPlayer = this.playerOne;
 };
 
-GameBoard.prototype.checkIfWinner = function(array){
-  var horizontalMatchCounter = 0;
-  var verticalMatchCounter = 0;
-  var previousValue = array[0];
-  for(var chipIndex = 0 ; chipIndex < array.length; chipIndex++){
-    //horizontal
-    if(previousValue[0] === array[chipIndex][0]){
-      horizontalMatchCounter++;
-      if(horizontalMatchCounter === 4){
-        console.log('winner!')
-        this.gameOver = true;
-      }
-    } else {
-      horizontalMatchCounter = 0;
+GameBoard.prototype.checkIfWinner = function (array) {
+    var horizontalMatchCounter = 0;
+    var verticalMatchCounter = 0;
+    var previousValue = array[0];
+    for (var chipIndex = 0; chipIndex < array.length; chipIndex++) {
+        //horizontal
+        if (previousValue[0] === array[chipIndex][0]) {
+            horizontalMatchCounter++;
+            if (horizontalMatchCounter === 4) {
+                console.log('winner!')
+                this.gameOver = true;
+            }
+        } else {
+            horizontalMatchCounter = 0;
+        }
+
+        //vertical match
+        if (previousValue[1] === array[chipIndex][1]) {
+            verticalMatchCounter++;
+            if (verticalMatchCounter === 4) {
+                console.log('winner!')
+                this.gameOver = true;
+            }
+        } else {
+            verticalMatchCounter = 0;
+        }
+
+        previousValue = array[chipIndex];
     }
+};
 
-    //vertical match
-    if(previousValue[1] === array[chipIndex][1]){
-      verticalMatchCounter++;
-      if(verticalMatchCounter === 4){
-        console.log('winner!')
-        this.gameOver = true;
-      }
-    } else {
-      verticalMatchCounter = 0;
-    }
-
-    previousValue = array[chipIndex];
-  }
-}
-
-function Chip(filled, player){
-  this.filled = filled;
-  this.player = player
+function Chip(filled, player) {
+    this.filled = filled;
+    this.player = player
 }
 
 /************************************************
  ********* Disc Cursor Above Game Board *********
  ***********************************************/
+
 function diskDropInit() {
 
     $(".preDropDisk").css("visibility", "hidden");
 
     function showDisk(disk) {
         disk.addClass('animated fadeIn');
-        disk.css("visibility", "visible");
+        disk.css({
+            "visibility": "visible",
+            "background-image": "url('images/disks/redDisk.png')",
+            "background-size": "cover"
+        });
     }
 
     function hideDisk(disk) {
