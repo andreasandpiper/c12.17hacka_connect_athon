891bd9bdde1bc5c94baf2a8d18c8ac663ae804bf
Andrea <andrea.wayte@gmail.com>
2018-01-04 07:20:44 -0800

    users cannot drop chip in filled column, game ends when entire board is filled

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ function initializeApp() {
     $(".game_board").on("click", ".square", newGame.columnClicked.bind(newGame));
     //function for clickedColumn = newGame.columnClicked.bind(newGame)
     diskDropInit();
-
 }
 
 var newGame;
@@ -36,7 +35,6 @@ function GameBoard() {
         verticalPicks: [],
         horizontalPicks: [],
         playerColor: 'blue'
-
     };
     this.currentPlayer = this.playerOne;
     this.createGameBoard = function (width, height) {
@@ -51,14 +49,15 @@ function GameBoard() {
 
 
 GameBoard.prototype.columnClicked = function (event) {
+  //get which column was clicked from the class
+  var targetColumn = $(event.target).attr("class").split(" ")[1];
+  var column = targetColumn[targetColumn.length - 1];
     //check to make sure only one animation at a time
-    if (this.pickedColumn || this.gameOver) {
+    //check if there are open spots in the column
+    if (this.pickedColumn || this.gameOver || this.board[0][column].filled) {
         return
     }
     this.pickedColumn = true;
-    //get which column was clicked from the class
-    var targetColumn = $(event.target).attr("class").split(" ")[1];
-    var column = targetColumn[targetColumn.length - 1];
     //drop the chip
     this.chipDrop(column);
 };
@@ -85,10 +84,10 @@ GameBoard.prototype.chipDrop = function (column) {
             this.showChip(column, row);
             //alternates players
             if (this.currentPlayer === this.playerOne) {
-                this.fillMatrixWithChip(vertPosition, horizPosition);
+                this.CheckIfWinner(vertPosition, horizPosition);
                 this.currentPlayer = this.playerTwo;
             } else {
-                this.fillMatrixWithChip(vertPosition, horizPosition);
+                this.CheckIfWinner(vertPosition, horizPosition);
                 this.currentPlayer = this.playerOne;
             }
             this.changeColor();
@@ -97,61 +96,26 @@ GameBoard.prototype.chipDrop = function (column) {
     }
 };
 
-GameBoard.prototype.fillMatrixWithChip = function(vertPosition, horizPosition){
+GameBoard.prototype.CheckIfWinner = function(vertPosition, horizPosition){
   this.currentPlayer.verticalPicks.push(vertPosition)
   this.currentPlayer.horizontalPicks.push(horizPosition)
-  this.checkIfWinner(this.currentPlayer.horizontalPicks);
-  this.checkIfWinner(this.currentPlayer.verticalPicks);
+  this.checkIfXYWinner(this.currentPlayer.horizontalPicks);
+  this.checkIfXYWinner(this.currentPlayer.verticalPicks);
   this.checkIfDiagonalWinner(this.currentPlayer.horizontalPicks.sort(), "+");//decreasing matches
   this.checkIfDiagonalWinner(this.currentPlayer.horizontalPicks.sort().reverse(), "-");//increasing matches
+  var newChip = new Chip(false, null);
 
+  var entireGameBoardFilled = this.board[0].every(gameBoardFilled);
+  if(entireGameBoardFilled){
+    console.log('cats game');
+  }
 }
 
-GameBoard.prototype.showChip = function (column, row) {
-    $gameSquare = $('.square.col' + column + '.row' + row);
-    $gameSquare.css({
-        "background-image": "url('../c12.17hacka_connect_athon/images/disks/" + this.currentPlayer.playerColor + "Disk.png')",
-        "background-size": "contain"
-    });
-    $gameSquare.addClass("fallToRow" + row);
-    this.changeColor();
-};
+function gameBoardFilled(chip) {
+  return chip.filled === true;
+}
 
-GameBoard.prototype.getPlayerNames = function () {
-    this.playerOne.name = "BRUTUS";
-    this.playerTwo.name = "CEASAR";
-    this.currentPlayer = this.playerOne;
-
-    $('#player1').text(this.playerOne.name).addClass('neonText-'+this.playerOne.playerColor);
-    $('#player2').text(this.playerTwo.name);
-};
-
-GameBoard.prototype.changeColor = function(){
-
-    if(this.currentPlayer === this.playerOne){
-        $('#player1').addClass('neonText-'+this.playerOne.playerColor);
-        $('#player2').removeClass('neonText-'+this.playerTwo.playerColor);
-    } else {
-        $('#player2').addClass('neonText-'+this.playerTwo.playerColor);
-        $('#player1').removeClass('neonText-'+this.playerOne.playerColor);
-    }
-
-    var $disk = $(".preDropDisk");
-
-    setTimeout(function () {
-        $disk.addClass('animated fadeOut');
-    }, 1000);
-    $disk.removeClass('animated fadeIn');
-    setTimeout(function () {
-        $disk.removeClass('animated fadeOut');
-        setTimeout(function () {
-            $disk.css("background-image", "url('images/disks/"+ newGame.currentPlayer.playerColor + "Disk.png')");
-            $disk.removeClass('animated fadeIn');
-        }, 1);
-    }, 1000);
-};
-
-GameBoard.prototype.checkIfWinner = function (array) {
+GameBoard.prototype.checkIfXYWinner = function (array) {
     array = array.sort();
     var matchCounter = 1;
     var previousValue = array[0];
@@ -160,23 +124,18 @@ GameBoard.prototype.checkIfWinner = function (array) {
         if (previousValue[0] === array[chipIndex][0] && (parseInt(previousValue[1]) + 1) == parseInt(currentValue[1]) ) {
             matchCounter++;
             if (matchCounter === 4) {
-                console.log('winner!')
                 victoryModal();
-
-
                 this.gameOver = true;
             }
         } else {
             matchCounter = 1;
         }
-
         previousValue = array[chipIndex];
       }
     previousValue = array[chipIndex];
-
 };
 
-function incrementOrDecrement(currentValue, upOrDown){
+GameBoard.prototype.incrementOrDecrement = function(currentValue, upOrDown){
   var types = {
     '+': function(){
       currentValue += 1;
@@ -195,13 +154,12 @@ GameBoard.prototype.checkIfDiagonalWinner = function (array, upOrDown){
     var currentChipRow = parseInt(array[chipIndex][0]);
     var currentChipCol = parseInt(array[chipIndex][1]);
     for(var compareChip = chipIndex ; compareChip< array.length ; compareChip++){
-      var lookForChip = (incrementOrDecrement(currentChipRow, upOrDown)).toString() + (currentChipCol+1).toString();
+      var lookForChip = (this.incrementOrDecrement(currentChipRow, upOrDown)).toString() + (currentChipCol+1).toString();
       if(array.indexOf(lookForChip) !== -1){
         diagonalMatchCounter++;
-        currentChipRow = incrementOrDecrement(currentChipRow, upOrDown);
+        currentChipRow = this.incrementOrDecrement(currentChipRow, upOrDown);
         currentChipCol++;
         if(diagonalMatchCounter === 4){
-          console.log('diagonal winner!')
           victoryModal();
           this.gameOver = true;
         }
@@ -212,6 +170,46 @@ GameBoard.prototype.checkIfDiagonalWinner = function (array, upOrDown){
     }
   }
 }
+
+GameBoard.prototype.showChip = function (column, row) {
+    $gameSquare = $('.square.col' + column + '.row' + row);
+    $gameSquare.css({
+        "background-image": "url('../c12.17hacka_connect_athon/images/disks/" + this.currentPlayer.playerColor + "Disk.png')",
+        "background-size": "contain"
+    });
+    $gameSquare.addClass("fallToRow" + row);
+    this.changeColor();
+};
+
+GameBoard.prototype.getPlayerNames = function () {
+    this.playerOne.name = "BRUTUS";
+    this.playerTwo.name = "CEASAR";
+    this.currentPlayer = this.playerOne;
+    $('#player1').text(this.playerOne.name).addClass('neonText-'+this.playerOne.playerColor);
+    $('#player2').text(this.playerTwo.name);
+};
+
+GameBoard.prototype.changeColor = function(){
+    if(this.currentPlayer === this.playerOne){
+        $('#player1').addClass('neonText-'+this.playerOne.playerColor);
+        $('#player2').removeClass('neonText-'+this.playerTwo.playerColor);
+    } else {
+        $('#player2').addClass('neonText-'+this.playerTwo.playerColor);
+        $('#player1').removeClass('neonText-'+this.playerOne.playerColor);
+    }
+    var $disk = $(".preDropDisk");
+    setTimeout(function () {
+        $disk.addClass('animated fadeOut');
+    }, 1000);
+    $disk.removeClass('animated fadeIn');
+    setTimeout(function () {
+        $disk.removeClass('animated fadeOut');
+        setTimeout(function () {
+            $disk.css("background-image", "url('images/disks/"+ newGame.currentPlayer.playerColor + "Disk.png')");
+            $disk.removeClass('animated fadeIn');
+        }, 1);
+    }, 1000);
+};
 
 function Chip(filled, player) {
     this.filled = filled;
